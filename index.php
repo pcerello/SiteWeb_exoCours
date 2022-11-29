f9f91098c3aac0e9cb8c0c6a86cee5d00d6f37df
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2022-11-29 15:25:38 +0100

    modif index

--- index.php
+++ index.php
@@ -4,29 +4,59 @@
             body{
                 text-align: center;
                 font-family: sans-serif;
+                background-color: lavender;
+            }
+            .colonne1{
+                display: flex;
+                flex-direction: column;
+                flex: 1;
+                background-image: url("imageFond.jpg");
+            }
+            .colonne2{
+                display: flex;
+                flex-direction: column;
+                flex: 1;
+            }
+            .ligne{
+                display: flex;
+                flex-direction: row;
+                flex: 1;
             }
         </style>
     </head>
     <body>
-        <h1>Mes TDs</h1>
-        <h2>PHP</h2>
-        <h3>TD1 : Les bases (1)</h3>
-        <a href="date.php">Date</a> 
-        <a href="multiplications.php">Multiplications</a> 
-        <h3>TD3 : Les bases (3)</h3>
-        <a href="calculatrice.php">Calculatrice</a>
-        <a href="calendrier.php">Calendrier</a>
-        <h3>TD3 : Base de données (1)</h3>
-        <a href="series.php">Les séries</a>
-        <a href="number.php">Liste des saisons</a>
-        <h3>TD4 : Base de données (3)</h3>
-        <a href="utilisateur.php">Inscription d'un utilisateur</a>
-        <h3>TD5 : Cookies et sessions</h3>
-        <a href="cookie.php">Cookie</a>
-        <a href="morpion.php">Morpion</a>
-        <a href="identification.php">Identification</a>
-        <h2>Frameworks</h2>
-        <h3>TD1 : Routage et vues</h3>
-        <p>Modifications effectuées dans le dossier symfony</p>
+        
+        <div class="ligne">
+            <div class="colonne1">
+                
+            </div>
+            <div class="colonne2">
+                <h1>Mes TDs</h1>
+                <h2>PHP</h2>
+                <h3>TD1 : Les bases (1)</h3>
+                <a href="date.php">Date</a> <br>
+                <a href="multiplications.php">Multiplications</a> 
+                <h3>TD3 : Les bases (3)</h3>
+                <a href="calculatrice.php">Calculatrice</a><br>
+                <a href="calendrier.php">Calendrier</a>
+                <h3>TD3 : Base de données (1)</h3>
+                <a href="series.php">Les séries</a><br>
+                <a href="number.php">Liste des saisons</a>
+                <h3>TD4 : Base de données (3)</h3>
+                <a href="utilisateur.php">Inscription d'un utilisateur</a>
+                <h3>TD5 : Cookies et sessions</h3>
+                <a href="cookie.php">Cookie</a><br>
+                <a href="morpion.php">Morpion</a><br>
+                <a href="identification.php">Identification</a>
+                <h2>Frameworks</h2>
+                <h3>TD1 : Routage et vues</h3>
+                <p>Modifications effectuées dans le dossier symfony</p>
+            </div>
+            
+            <div class="colonne1">
+                
+            </div>
+        </div>
+        
     </body>
 </html>
